2ffdb0c9c97cdfe8ad388c0b6c1333751f6afbb1
hyeju <kwonhyeju0531@gmail.com>
2020-08-21 17:12:28 +0900

    ãmodel id 추가

--- posts/models.py
+++ posts/models.py
@@ -6,3 +6,4 @@ class Posts(models.Model):
     title = models.CharField(verbose_name='TITLE', max_length=50)
     desc = models.CharField(max_length=100)
     content = models.TextField()
+    id = models.indexes()
