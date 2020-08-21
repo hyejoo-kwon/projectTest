92fd159fe8414d78990b2a511d552bccc0a1696e
hyeju <kwonhyeju0531@gmail.com>
2020-08-21 17:09:00 +0900

    last

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Posts(models.Model):
+    title = models.CharField(verbose_name='TITLE', max_length=50)
+    desc = models.CharField(max_length=100)
+    content = models.TextField()
