81a9f527faa5b0c23e0de99647bd28dd5eeae702
hyunsoo <hyunsoodii@gmail.com>
2020-08-21 17:16:08 +0900

    accounts

--- projectTest/projectTest/urls.py
+++ projectTest/projectTest/urls.py
@@ -15,7 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+from projectTest import UserCreateView, UserCreateDoneTV
 
 urlpatterns = [
+    path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
+
+    path('accounts/register/', UserCreateView.as_view(), name='register'),
+    path('accounts/register/done/', UserCreateDoneTV.as_view(), name='register_done'),
 ]
